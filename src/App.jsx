f5935a4656b93d637d05cdb38ca1b869c16d788f
Alberto <albertopimentel04@gmail.com>
2022-04-04 11:46:32 -0500

    Configuracion inicial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function App() {
+  console.log("Hola");
+  return (
+    <div className="App">
+      <h1>Hola</h1>
+    </div>
+  );
+}
+
+export default App;
